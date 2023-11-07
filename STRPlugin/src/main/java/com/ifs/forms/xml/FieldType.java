1ce664d9c895ca0cbbeb8e0ef01156c790f9e75c
Tami Boharon <tamibo@matrix.co.il>
2023-11-07 14:57:25 +0200

    init

--- STRPlugin/src/main/java/com/ifs/forms/xml/FieldType.java
+++
@@ -1,5 +0,0 @@
-package com.ifs.forms.xml;
-
-public enum FieldType {
-	String,Date
-}
--- STRPlugin/src/main/java/com/ifs/forms/xml/SetType.java
+++
@@ -1,5 +0,0 @@
-package com.ifs.forms.xml;
-
-public enum SetType {
-	BLANK,branch,dispositions,transactions
-}
